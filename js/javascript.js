5ce1a56337dba98798f83aca49a4d172856a5f5a
Franco Fitte <132512866+FrancoFitte@users.noreply.github.com>
2023-06-13 16:05:39 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+function cambiarfondo() {
+document.body.style.backgroundColor= "black";
+document.body.style.color= "white";
+document.getElementById("main").style.backgroundColor = "black";
+document.getElementById("main").style.color = "white";
+document.getElementById("header").style.backgroundColor = "black";
+document.getElementById("imagenportada").style.backgroundColor = "white";
+document.getElementById("parrafohistoria").style.backgroundColor = "black";
+document.getElementById("parrafohistoria").style.color = "white";
+document.getElementById("footer").style.backgroundColor = "black";
+}
+
+
+function copy() {
+    alert("Autor: Franco Fitte - email: francofitte90@gmail.com - Sierras Bayas, Argentina");
+    }
